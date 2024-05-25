0b2a661eddfe756802fcd0d9aba74cb13f24dced
Mike Foster <i.am.mike.foster@gmail.com>
2024-05-25 15:37:45 +0000

    return a TokenStream2 from expand (like from wraptestcase)

--- src/raises.rs
+++ src/raises.rs
@@ -1,28 +1,30 @@
 use proc_macro2::TokenStream as TokenStream2;
 use pyo3::{PyAny, PyErr};
 use syn::{parse_quote, Block, Expr, Ident, ItemStruct, Stmt};
+use quote::ToTokens;
 
 struct Pyo3Raises {
     err: Ident,
     block: Block,
 }
 
-fn expand(invocation: Pyo3Raises) -> Expr {
+fn expand(invocation: Pyo3Raises) -> TokenStream2 {
     let err = invocation.err;
     let block = invocation.block;
-    parse_quote! {
+    let with_block: Stmt = parse_quote! {
         match #block {
             Ok(_) => panic!("No Error"),
             Err(error) if error.is_instance_of::<#err>(py) => return (),
             Err(_) => panic!("Wrong Error"),
         }
-    }
+    };
+    with_block.into_token_stream()
 }
 
 #[cfg(test)]
 mod test {
     use pyo3::exceptions::PyTypeError;
-
+    use quote::quote;
     use super::*;
 
     #[test]
@@ -37,7 +39,7 @@ mod test {
             err: errortype,
             block: codeblock 
         };
-        let expected: Expr = parse_quote! {
+        let expected: TokenStream2 = quote! {
             match  {
                 addone.call1("4",)
             };  {
@@ -46,6 +48,6 @@ mod test {
                 Err(_) => panic! ("Wrong Error")
             }
         };
-        assert_eq!(expand(invocation), expected)
+        assert_eq!(expand(invocation).to_string(), expected.to_string())
     }
 }
