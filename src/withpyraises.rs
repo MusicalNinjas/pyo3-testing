c575a0f4f3c7075148dfb9819b169e3c7aed3894
Mike Foster <i.am.mike.foster@gmail.com>
2024-05-26 13:52:05 +0000

    add a nice error message (checked in IDE)

--- src/withpyraises.rs
+++ src/withpyraises.rs
@@ -8,7 +8,7 @@ use syn::{
     parse::{Parse, ParseStream},
     parse2, parse_quote,
     token::Comma,
-    Block, Ident, Stmt,
+    Block, Ident, Stmt
 };
 
 /// Parses the macro invocation contents as a with raises statement and then returns the
@@ -24,7 +24,7 @@ pub fn impl_with_py_raises(input: TokenStream2) -> TokenStream2 {
 /// Represents a well-formed `with pytest.raises`-like statement.
 ///
 /// In order to be correctly parsed this should be in the form of
-/// `Error Type` `Comma: [,]` `{block in brackets}`  
+/// `Error Type` `Comma: [,]` `{block in braces}`  
 #[derive(Debug, PartialEq)]
 struct WithRaisesStmt {
     /// The error type, this must be the ident of a valid rust error type which is already in scope.
@@ -43,7 +43,17 @@ struct WithRaisesStmt {
 impl Parse for WithRaisesStmt {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         let err: Ident = input.parse()?;
-        let _comma: Comma = input.parse()?;
+        let _comma: Comma = match input.parse() {
+            Ok(comma) => comma,
+            Err(_) => return Err(
+                syn::Error::new(
+                    err.span(),
+                    "Expected a comma (`,`) after this:
+                    Correct format for with_py_raises is: `Error Type` `Comma: [,]` `{block in braces}`
+                    E.g.: `with_py_raises!(PyTypeError, { addone.call1((\"4\",)) })`"
+                )
+            )
+        };
         let block: Block = input.parse()?;
         Ok(WithRaisesStmt { err, block })
     }
