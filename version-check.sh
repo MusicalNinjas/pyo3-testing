bd3cb7aad7443208b4cfbb17285465954f6ecc9e
Mike Foster <i.am.mike.foster@gmail.com>
2026-06-13 14:13:54 +0000

    only need first non-zero part of version

--- version-check.sh
+++ version-check.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
 pyo3="$(jaq -cr .dependencies.pyo3 Cargo.toml)"
-pyo3="${pyo3#0.}"
-pyo3="${pyo3%.*}"
+pyo3="${pyo3#0.}" # strip leading `0.` if present
+pyo3="${pyo3%%.*}" # strip everything after the next `.`
 
 echo "pyo3 version: $pyo3"
 
 pyo3_testing="$(jaq -cr .package.version Cargo.toml)"
 pyo3_testing="${pyo3_testing#0.}"
-pyo3_testing="${pyo3_testing%.*}"
+pyo3_testing="${pyo3_testing%%.*}"
 
 echo "pyo3_testing version: $pyo3_testing"
 
