c0723dd25768bd789c56fe598dc4957b1bd00717
MusicalNinjaDad <102677655+MusicalNinjaDad@users.noreply.github.com>
2026-06-13 16:26:20 +0200

    Immutable releases (#40)
    - OIDC for crate.io
    - immutable release workflow
    - bash-based version check

---
+++ version-check.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+set -euo pipefail
+
+pyo3="$(jaq -cr .dependencies.pyo3 Cargo.toml)"
+pyo3="${pyo3#0.}" # strip leading `0.` if present
+pyo3="${pyo3%%.*}" # strip everything after the next `.`
+
+echo "pyo3 version: $pyo3"
+
+pyo3_testing="$(jaq -cr .package.version Cargo.toml)"
+pyo3_testing="${pyo3_testing#0.}"
+pyo3_testing="${pyo3_testing%%.*}"
+
+echo "pyo3_testing version: $pyo3_testing"
+
+[[ $pyo3_testing == $pyo3 ]]
