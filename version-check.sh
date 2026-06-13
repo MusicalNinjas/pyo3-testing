7ecb08652d114d2133e79a34cff2862dbf7b838a
Mike Foster <i.am.mike.foster@gmail.com>
2026-06-13 14:11:57 +0000

    strip leading 0. if present

--- version-check.sh
+++ version-check.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
 pyo3="$(jaq -cr .dependencies.pyo3 Cargo.toml)"
+pyo3="${pyo3#0.}"
 pyo3="${pyo3%.*}"
 
 echo "pyo3 version: $pyo3"
 
 pyo3_testing="$(jaq -cr .package.version Cargo.toml)"
+pyo3_testing="${pyo3_testing#0.}"
 pyo3_testing="${pyo3_testing%.*}"
 
 echo "pyo3_testing version: $pyo3_testing"
