1bdf9f0c714f92c36fd682ec914050cb744e7088
Mike Foster <i.am.mike.foster@gmail.com>
2026-06-13 14:09:12 +0000

    run version check in CI

---
+++ version-check.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+pyo3="$(jaq -cr .dependencies.pyo3 Cargo.toml)"
+pyo3="${pyo3%.*}"
+
+echo "pyo3 version: $pyo3"
+
+pyo3_testing="$(jaq -cr .package.version Cargo.toml)"
+pyo3_testing="${pyo3_testing%.*}"
+
+echo "pyo3_testing version: $pyo3_testing"
+
+[[ $pyo3_testing == $pyo3 ]]
